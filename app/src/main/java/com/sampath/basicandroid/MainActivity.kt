a4e0726aa1dd5fb1d49d46f22f75ebcc71853dc7
Sampath <sampathpranay02@gmail.com>
2021-05-15 15:59:48 +0530

    Alert Box

--- app/src/main/java/com/sampath/basicandroid/MainActivity.kt
+++ app/src/main/java/com/sampath/basicandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sampath.basicandroid
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
@@ -8,6 +9,8 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.alert.*
 import kotlinx.android.synthetic.main.button_toast.*
 import kotlinx.android.synthetic.main.listview.*
 import kotlinx.android.synthetic.main.tts_layout.*
@@ -16,6 +19,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.alert)
         setContentView(R.layout.button_toast)
         setContentView(R.layout.tts_layout)
         setContentView(R.layout.listview)
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        //Text to Speech Conversion
+        // Text to Speech Conversion
         var ttsText = tts_text
         var ttsButton = tts_button
         lateinit var tts : TextToSpeech
@@ -68,15 +72,34 @@ class MainActivity : AppCompatActivity() {
              );
          }
 
+        // Basic Listview using ArrayAdapter
         val array = arrayOf("United states", "Japan", "China", "Sri Lanka", "Russia", "Europe",
             "Spain", "Swiss", "Denmark", "Australia", "London", "West Indies", "Iraq", "Kabul",
             "Dubai", "Israel", "Nepal", "Iran")
 
-            val adapter = ArrayAdapter(this,
+            val adapter = ArrayAdapter(this@MainActivity,
                 R.layout.listview_item, array)
 
             val listView: ListView = listview
             listView.setAdapter(adapter)
+
+        // Alert
+        btnShowAlert.setOnClickListener{
+
+            val dialogBuilder = AlertDialog.Builder(this@MainActivity)
+            dialogBuilder.setMessage("Do you want to proceed?")
+
+                .setPositiveButton("Yes") { _, _ ->
+                    finish()
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+
+            val alert = dialogBuilder.create()
+            alert.setTitle("Alert")
+            alert.show()
+        }
 
     }
 }
